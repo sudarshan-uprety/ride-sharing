a612f0476b3b562da6895c72c9c35e727c7cd425
sudarshan-uprety <mail@sudarshan-uprety.com.np>
2025-05-02 09:04:25 -0700

    fixing schemas

--- schemas/user.go
+++ schemas/user.go
@@ -1,6 +1,22 @@
 package schemas
 
+type LoginRequest struct {
+	Email    string `json:"email" valid:"required,email"`
+	Password string `json:"password" valid:"required,stringlength(8|50)"`
+}
+
+type LoginResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
 type AuthInput struct {
 	Email    string `json:"email" valid:"required,email"`
 	Password string `json:"password" valid:"required,stringlength(6|100)"`
+}
+
+type RegisterRequest struct {
+}
+
+type RegisterResponse struct {
 }
