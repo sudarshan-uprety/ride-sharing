a7c54a300dedea3fef539fe2ce166a8e8aae4790
sudarshan-uprety <mail@sudarshan-uprety.com.np>
2025-05-09 00:28:25 -0700

    adding provider in both rider and admin as well

---
+++ internal/domains/riders/provider/provider.go
@@ -0,0 +1,20 @@
+package provider
+
+// import (
+// 	"context"
+
+// 	"ride-sharing/internal/domains/riders/repository"
+// 	"ride-sharing/internal/pkg/auth"
+// )
+
+// type UserProvider struct {
+// 	repo repository.UserRepository
+// }
+
+// func NewUserProvider(repo repository.UserRepository) auth.UserProvider {
+// 	return &UserProvider{repo: repo}
+// }
+
+// func (p *UserProvider) GetByID(ctx context.Context, id string) (interface{}, error) {
+// 	return p.repo.GetByID(ctx, id)
+// }
