1cffc55e5ef203145de71777bc2e28b32c0f5344
sudarshan-uprety <mail@sudarshan-uprety.com.np>
2025-05-16 10:29:19 -0700

    fixing swagger

--- docs/docs.go
+++ docs/docs.go
@@ -628,8 +628,8 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
-	BasePath:         "/api/v1",
+	Host:             "",
+	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "Ride Sharing Auth API",
 	Description:      "This is a ride sharing service API.",
