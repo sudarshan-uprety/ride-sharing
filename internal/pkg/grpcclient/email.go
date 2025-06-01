7e77e3523c9f3586befa57ef24fa50c703ee6efc
sudarshan-uprety <mail@sudarshan-uprety.com.np>
2025-06-01 00:32:16 -0700

    fall back option for kafka

--- internal/pkg/grpcclient/email.go
+++ internal/pkg/grpcclient/email.go
@@ -2,8 +2,11 @@ package grpcclient
 
 import (
 	"context"
+	"errors"
+	"math"
 	"ride-sharing/config"
 	"ride-sharing/internal/proto"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -11,7 +14,8 @@ import (
 
 type NotificationClient struct {
 	client proto.NotificationServiceClient
-	conn   *grpc.ClientConn
+	// kafka  *kafka.Producer
+	conn *grpc.ClientConn
 }
 
 func NewNotificationClient(cfg *config.Config) (*NotificationClient, error) {
@@ -33,16 +37,45 @@ func (c *NotificationClient) Close() error {
 	return c.conn.Close()
 }
 
-func (n *NotificationClient) SendRegisterEmail(ctx context.Context, to string, otp string) (*proto.StandardResponse, error) {
+func (n *NotificationClient) SendRegisterEmail(ctx context.Context, to string, otp string) (bool, error) {
 	req := &proto.RegisterEmailRequest{
 		To:  to,
 		Otp: otp,
 	}
 
-	resp, err := n.client.SendRegisterEmail(ctx, req)
-	if err != nil {
-		return nil, err
+	for attempt := 1; attempt <= 3; attempt++ {
+		_, err := n.client.SendRegisterEmail(ctx, req)
+		if err == nil {
+			return true, nil
+		}
+		// lastErr := err
+		time.Sleep(time.Duration(math.Pow(2, float64(attempt))) * time.Second)
 	}
 
-	return resp, nil
+	// err := n.kafka.Produce("notification-topic", map[string]string{
+	// 	"type": "register_email",
+	// 	"to":   to,
+	// 	"otp":  otp,
+	// })
+	// if err != nil {
+	// 	log.Printf("Failed to enqueue message to Kafka: %v", err)
+	// 	return false, errors.New("all gRPC attempts failed and Kafka fallback also failed")
+	// }
+
+	return false, errors.New("email queued in Kafka due to notification service downtime")
+
 }
+
+// func (n *NotificationClient) SendForgetPasswordEmail(ctx context.Context, to string, otp string) (bool, error) {
+// 	req := &proto.ForgetPasswordEmailRequest{
+// 		To:  to,
+// 		Otp: otp,
+// 	}
+
+// 	resp, err := n.client.SendForgetPasswordEmail(ctx, req)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return resp, nil
+// }
